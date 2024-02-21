fae4a7c68017716ab21f1ef12b9328eeadca45a0
jrodriguez-wm <jorge.rodriguez@webmaps.com.mx>
2024-02-20 19:44:04 -0600

    Se agregan imagenes a utilizar y se crean los estilos de backgroun y de texto
    Se agrega comando para compilar proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [    './public/index.html',  ],
+  // purge:[],
+  // darkMode: false,
+  content: ['./public/index.html',],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+			  'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+			  'yosemite': "url('../img/yosemite.jpg')",
+			  'LA': "url('../img/LA.jpg')",
+			  'seattle': "url('../img/seattle.jpg')",
+			  'new_york': "url('../img/new_york.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'sydney': "url('../img/sydney.jpg')",
+			  'miami': "url('../img/miami.jpg')",
+			  'switzerland': "url('../img/switzerland.jpg')",
+			  'bali': "url('../img/bali.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'chicago': "url('../img/chicago.jpg')",
+			  'europe': "url('../img/europe.jpg')",
+			  'iceland': "url('../img/iceland.jpg')",
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+				'secondary': '#8FA206',
+				'terciary': '#61AEC9',
+      }),
+      textColor:{
+        'principal': '#CC2D4A',
+				'secondary': '#8FA206',
+				'tertiary': '#61AEC9',
+      }
+    },
+  },
+  variants:{
+    extend:{},
   },
   plugins: []
 }
