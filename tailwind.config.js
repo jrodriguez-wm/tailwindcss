388a0577e321d6dc27d199ee510f98ce04f34e2a
jrodriguez-wm <jorge.rodriguez@webmaps.com.mx>
2024-02-22 23:55:25 -0600

    Se agregan estilos para darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // purge:[],
-  // darkMode: false,
+  darkMode: 'class',
   content: ['./public/index.html',],
   theme: {
     extend: {
